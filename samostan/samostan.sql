dbca4bda2240b863093e48daaf16ed8e9f18653e
Marko Zupanic <104766005+markozupanic@users.noreply.github.com>
2022-05-11 13:57:04 +0200

    dodani tipovi podataka i vanjski ključevi
    +slike dijagrama u Dbeaveru

--- samostan/samostan.sql
+++ samostan/samostan.sql
@@ -1,6 +1,6 @@
 #C:\xampp\mysql\bin\mysql -uroot < C:\Users\dell\Documents\EdunovaJP26\SQL\edunovajp26.sql
 #C:\xampp\mysql\bin\mysql -uroot < C:\Korisnici\X\Dokumenti\GitHub\baze\zadaca2\doktorskaordinacija.sql
-#C:\xampp\mysql\bin>mysql -uroot <C:\Users\X\Documents\GitHub\baze\samostan.sql
+#C:\xampp\mysql\bin>mysql -uroot < C:\Users\X\Documents\GitHub\baze\samostan\samostan.sql
 
 
 
@@ -9,22 +9,32 @@ create database samostan;
 use samostan;
 
 create table samostan(
-    brojsvecenika varchar(50),
+    sifra int not null primary key auto_increment,
+    brojsvecenika int not null,
     adresa varchar(50),
     radnovrijeme varchar(50),
-    vrstasamostana varchar(50)
+    vrstasamostana varchar(50),
+    svecenik int not null,
+    posao int not null
 );
 
 create table svecenik(
+    sifra int not null primary key auto_increment,
     osbine varchar(50),
-    starost varchar(50),
-    ime varchar(50),
-    prezime varchar(50)
+    starost int ,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    posao int not null,
+    samostan int not null
 );
 
 create table posao(
-    vrstaposla varchar(50)
+    sifra int not null primary key auto_increment,
+    vrstaposla varchar(50),
+    samostan int not null
 );
 
 
-
+alter table svecenik add foreign key (samostan) references samostan(sifra);
+alter table posao add foreign key (samostan) references samostan(sifra);
+alter table svecenik add foreign key (posao) references posao(sifra);
