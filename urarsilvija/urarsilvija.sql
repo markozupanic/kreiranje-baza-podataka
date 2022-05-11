f0963d59d15b5feb5defba323385a1f3c02946e8
Marko Zupanic <104766005+markozupanic@users.noreply.github.com>
2022-05-11 13:45:54 +0200

    dodani tipovi podataka i vanjski ključevi
    +slike dijagrama u Dbeaveru

--- urarsilvija/urarsilvija.sql
+++ urarsilvija/urarsilvija.sql
@@ -1,6 +1,6 @@
 #C:\xampp\mysql\bin\mysql -uroot < C:\Users\dell\Documents\EdunovaJP26\SQL\edunovajp26.sql
 #C:\xampp\mysql\bin\mysql -uroot < C:\Korisnici\X\Dokumenti\GitHub\baze\zadaca2\doktorskaordinacija.sql
-#C:\xampp\mysql\bin>mysql -uroot <C:\Users\X\Documents\GitHub\baze\urarsilvija.sql
+#C:\xampp\mysql\bin>mysql -uroot < C:\Users\X\Documents\GitHub\baze\urarsilvija\urarsilvija.sql
 
 
 
@@ -9,29 +9,36 @@ create database urarsilvija;
 use urarsilvija;
 
 create table sat(
-    marka varchar(50),
-    godinaproizvodnje varchar(50),
-    vrstasata varchar(50),
-    ostecenje varchar(50)
+    sifra int not null primary key auto_increment,
+    marka varchar(50) not null,
+    godinaproizvodnje datetime(4) not null,
+    vrstasata varchar(50) null,
+    ostecenje varchar(50) not null,
+    korisnik int not null,
+    osoba int not null
 );
 
 create table korisnik(
-    ime varchar(50),
-    prezime varchar(50),
-    telefon varchar(50),
-    budzet varchar(50)
+    sifra int not null primary key auto_increment,
+    osoba int not null,
+    telefon int not null,
+    budzet int not null
 );
 
 create table urar(
-    ime varchar(50),
-    prezime varchar(50),
+    sifra int not null primary key auto_increment,
+    osoba int not null,
     znanje varchar(50),
     cijena varchar(50)
 );
 
 create table osoba(
-    ime varchar(50),
-    prezime varchar(50)
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null ,
+    prezime varchar(50) null
 );
 
-
+alter table sat add foreign key (korisnik) references korisnik(sifra);
+alter table sat add foreign key (osoba) references osoba(sifra);
+alter table urar add foreign key (osoba) references osoba(sifra);
+alter table korisnik add foreign key (osoba) references osoba(sifra);
