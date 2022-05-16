9dbad0df2070664429c5093e175cfe9f9d010ad7
Marko Zupanic <104766005+markozupanic@users.noreply.github.com>
2022-05-16 17:13:12 +0200

    definiranje vanjskih ključeva
    +ER dijagram

--- nakladnik/nakladnik.sql
+++ nakladnik/nakladnik.sql
@@ -9,17 +9,34 @@ create table izdavac(
     ime varchar(50),
     prezime varchar(50),
     znanje varchar(50),
-    placa int 
+    placa int,
+    umjetnik int not null
 );
 
 create table djela(
+    sifra int not null primary key auto_increment,
     ime varchar(50),
     cijena int,
-    vrstadjela varchar(50)
+    vrstadjela varchar(50),
+    umjetnik int not null
 );
 create table mjesto(
+    sifra int not null primary key auto_increment,
     adresa varchar(50),
     velicinamjesta int
 );
+
+create table umjetnik(
+    sifra int not null primary key auto_increment,
+    ime varchar(50),
+    prezime varchar(50),
+    starost int,
+    mjesto int not null
+);
+
+alter table djela add foreign key (umjetnik) references umjetnik(sifra);
+alter table izdavac add foreign key (umjetnik) references umjetnik(sifra);
+
+alter table umjetnik add foreign key (mjesto) references mjesto(sifra);
 
 
