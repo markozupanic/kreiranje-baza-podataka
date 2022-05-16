4a704ff83c5c88572403a4b9c78c11371ee42597
Marko Zupanic <104766005+markozupanic@users.noreply.github.com>
2022-05-16 16:39:49 +0200

    definiranje vanjskih kljućeva
    +ER dijagram u Dbeaveru

--- osnovnaskola/osnovnaskola.sql
+++ osnovnaskola/osnovnaskola.sql
@@ -15,7 +15,8 @@ create table dijete(
     sifra int not null primary key auto_increment,
     ime varchar(50),
     prezime varchar(50),
-    dob int
+    dob int,
+    osnovnaskola int not null
     );
 
 create table ucteljica(
@@ -29,5 +30,24 @@ create table radionica(
     sifra int not null primary key auto_increment,
     vrsta varchar(50),
     trajanjeradionice int not null,
-    pocetak datetime null
+    pocetak datetime null,
+    osnovnaskola int not null,
+    ucteljica int not null
 );
+
+create table program(
+    pocetak datetime,
+    krajprograma datetime,
+    brojpolaznika int,
+    dijete int not null,
+    radionica int not null
+);
+
+
+
+alter table program add foreign key (dijete) references dijete(sifra);
+alter table program add foreign key (radionica) references radionica(sifra);
+
+alter table dijete add foreign key (osnovnaskola) references osnovnaskola(sifra);
+alter table radionica add foreign key (osnovnaskola) references osnovnaskola(sifra);
+alter table radionica add foreign key (ucteljica) references ucteljica(sifra);
