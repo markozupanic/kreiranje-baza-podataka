7709e82586dec446d5c73f9fa9feb88ca83725b2
Marko Zupanic <104766005+markozupanic@users.noreply.github.com>
2022-05-23 16:30:48 +0200

    vjezba

--- fakultet/fakultet.sql
+++ fakultet/fakultet.sql
@@ -30,13 +30,24 @@ create table rok(
 
 create table ispit(
     sifra int not null primary key auto_increment,
-    datumispita datetime,
-    trajanjeispita int,
-    brojispita int,
+    datumispita datetime not null,
+    trajanjeispita int not null,
+    brojispita int not null,
     kolegij int not null
 );
 
+insert into kolegij (sifra,naziv,semestar,rok)
+values (null,'poslovbbna informatika' ,'drugi',3);
 
 alter table student add foreign key (ispit) references ispit(sifra);
 alter table rok add foreign key (ispit) references ispit(sifra);
 alter table ispit add foreign key (kolegij) references kolegij(sifra);
+
+
+insert into ispit (sifra,datumispita,trajanjeispita,brojispita,kolegij)
+values (null,'2022-03-02',2,6,1);
+
+
+
+insert into student (sifra,ime,prezime,jmbg,kolegij,ispit) 
+values (null,'marko','zupanic',186484184,1,1);
