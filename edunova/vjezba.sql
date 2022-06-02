705f7d8781ac7911a5e119751e93073f92007d8b
Marko Zupanic <104766005+markozupanic@users.noreply.github.com>
2022-06-02 22:55:31 +0200

    naredbe

--- edunova/vjezba.sql
+++ edunova/vjezba.sql
@@ -1,3 +1,118 @@
+select naziv, smjer,predavac from grupa;
+
+select a.naziv as grupa, 
+b.naziv as smjer, d.ime, d.prezime
+from grupa a inner join smjer b
+on a.smjer=b.sifra
+left join predavac c
+on a.predavac = c.sifra
+left join osoba d
+on c.osoba = d.sifra
+
+# Izlistajte sva imena i prezimena 
+# polaznika na grupi JP26
+
+# Zadatak: Sebe upišite u grupu PP25
+select * from polaznik where osoba=6;
+select * from osoba where ime='Ivana';
+select * from grupa;
+insert into clan(grupa,polaznik)
+values (2,4);
+
+# Izlistajte sva imena i prezimena 
+# polaznika na grupi PP25
+
+# Obrišite sebe iz grupe PP25
+
+# Postavite sebi broj ugovora 12/2022
+
+
+# baza knjižnica
+# Izlistajte sve knjige s ljubavnom tematikom
+
+select * from katalog where naslov like '%ljub%';
+
+# na postojeće rezultate dovucite 
+# naziv mjesta umjesto šifre
+
+select a.naslov, b.naziv
+from katalog a inner join mjesto b
+on a.mjesto =b.sifra
+where a.naslov like '%ljub%';
+
+# Čitati: https://dev.to/azibom/sql-joins-29j4
+
+# Izlistajte sve izdavače koji su 
+# Društvo s ograničenom odgovornošću
+select * from izdavac where naziv like '%d%o%o%';
+
+# Unesite sebe kao autora
+
+# Objavite knjigu (vašu omiljenu), 
+# izdavač i mjesto po izboru
+
+# Postavite sebi kao autoru datum rođenja na danas
+
+# OVO VAM NISAM POKAZAO
+# Tablici katalog dodajte kolonu cijena
+# Svakoj knjizi postavite slučajnu vrijednost između
+# 100 i 200
+
+
+# izvucite sve nazive knjiga koje su izdali
+# ne aktivni izdavači
+
+
+# Izvucite sve autore koji nemaju 
+# definiran datum rođenja
+select * from autor where datumrodenja is null;
+
+# Izvucite sve autore 
+# koji nisu izdali niti jednu knjigu
+select a.ime, a.prezime 
+from autor a left join katalog b
+on b.autor =a.sifra 
+where b.naslov is null
+
+
+select * from smjer;
+
+# filtriranje kolona - select liste
+# 1. * - sve kolone
+# 2. nazivi kolina odvojeni zarezom
+# 3. konstanta
+# 4. izraz
+select naziv,cijena from smjer;
+select naziv,cijena,naziv from smjer;
+select naziv, 'Osijek', 10 from smjer;
+# zamjenska imena kolona
+select naziv as smjer, 'Edunova' as skola from smjer;
+select naziv, now() as vrijeme from smjer;
+
+select *, naziv, 'Osijek' from smjer;
+
+
+# filtriranje redova
+select * from osoba where sifra=1;
+
+# operatori where klauzule
+# operatori uspoređivanja: = != (<>) > >= < <=
+select * from osoba where sifra<=4;
+# logički operatori: and or not (https://i.ytimg.com/vi/7dvqfpXEjdg/maxresdefault.jpg)
+select * from osoba where sifra>3 and sifra<5;
+select * from osoba where sifra=3 or sifra=5;
+select * from osoba where not sifra=3;
+# ostali operatori: like in between
+select * from osoba where ime='Marko';
+select * from osoba where ime like 'M%';
+select * from osoba where ime like '%a';
+select * from osoba where prezime like '%r%';
+
+select * from osoba where sifra=2 or sifra=6 or sifra=9;
+select * from osoba where sifra in (2,6,9);
+
+select * from osoba where sifra>=5 and sifra<=7;
+select * from osoba where sifra between 5 and 7;
 
 
 select * from products
